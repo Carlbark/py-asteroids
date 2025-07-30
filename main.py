763526803701e91ff6510a0c6012a6a5a19a0ae1
carlbark <markus.carlbark@gmail.com>
2025-07-30 14:47:52 +0200

    created asteroids spawning in a field

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -18,6 +20,10 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = updateable, drawable
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = asteroids, updateable, drawable
+    AsteroidField.containers = updateable
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
