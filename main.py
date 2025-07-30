3e8656222ff4a44c9c702cef0876cb53996f1938
carlbark <markus.carlbark@gmail.com>
2025-07-30 14:10:27 +0200

    use sprite groups for player state

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updateable, drawable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        screen.fill("black")
+        updateable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -3,6 +3,9 @@ from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
+    
+    """Player class representing the spaceship in the game."""
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
